3468bff5bcfd5f8c59302f529f5faac743c56253
BrighamV <val16012@byui.edu>
2020-11-12 22:17:06 -0700

    added scavenger hunt 3rd fragment to navigation

--- BuildEnvironment/app/src/main/java/com/example/h2project/ScavengerHuntFragment2.java
+++ BuildEnvironment/app/src/main/java/com/example/h2project/ScavengerHuntFragment2.java
@@ -38,5 +38,12 @@ public class ScavengerHuntFragment2 extends Fragment {
                         .navigate(R.id.action_scavengerHuntFragment2_to_scavengerHuntFragment1);
             }
         });
+        view.findViewById(R.id.scavenger_hunt_fragment_2_button_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ScavengerHuntFragment2.this)
+                        .navigate(R.id.action_scavengerHuntFragment2_to_scavengerHuntFragment3);
+            }
+        });
     }
 }
